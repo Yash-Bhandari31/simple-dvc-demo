eaaa77cfe617a6896faf50fb52639a1392d40907
bhagwat-chate <bhagwat.chate25@gmail.com>
2021-06-26 11:06:59 +0530

    two more pytest examples

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,10 +5,16 @@ class NotInRange(Exception):
         self.message = message
         super().__init__(self.message)
 
-def test_generic():
-    a = 1
-
     with pytest.raises(NotInRange):
         if a not in range(10,20):
             print('Exception occured')
             raise NotInRange
+
+def hello():
+    a = 1
+    assert True
+    
+def test_ing():
+    a = 2
+    b = 2
+    assert True
